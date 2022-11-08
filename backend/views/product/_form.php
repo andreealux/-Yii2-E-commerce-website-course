e20dbf286dc5397a7a3126ec697086a6baf94c00
Andreea <nenciucrasi.andreea@gmail.com>
2022-11-08 12:47:10 +0200

    creates login form, migrations, models and crud, customizes product form

---
+++ backend/views/product/_form.php
@@ -0,0 +1,48 @@
+<?php
+
+use dosamigos\ckeditor\CKEditor;
+use yii\helpers\Html;
+use yii\bootstrap5\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\Product $model */
+/** @var yii\bootstrap5\ActiveForm $form */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->widget(CKEditor::class, [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>
+
+    <?= $form->field($model, 'image', [
+            'template' => '
+                    <div class="custom-file">
+                        {input}
+                        {label}
+                        {error}
+                    </div>
+            ',
+        'labelOptions' => ['class' => 'custom-file-label'],
+        'inputOptions' => ['class' => 'custom-file-input']
+    ])->textInput(['type' => 'file']) ?>
+
+    <?= $form->field($model, 'price')->textInput([
+            'maxlength' => true,
+            'type' => 'number'
+    ]) ?>
+
+    <?= $form->field($model, 'status')->checkbox() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
